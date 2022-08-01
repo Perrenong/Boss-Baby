1ed6801ead4cd3950f634a1320be9c6fba90ae25
kahfon <S10220869@connect.np.edu.sg>
2022-08-01 14:22:23 +0800

    overheads

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -9,7 +9,8 @@ with open (r"\Boss_baby_PFB\Boss-Baby-1\project_group\csv_reports\overheads.csv"
         # print(line[0], line[1])
         Overheads.append(line)
 
-flag_list = []
+category = []
+
 highest_expense = "salary_expense"
     
 print(f"The highest category is salary_expense, 29.43"
