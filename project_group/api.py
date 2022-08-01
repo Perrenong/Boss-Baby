e5a953f9c138f758b95fabf36151f934c4c30d5a
kahfon <S10220869@connect.np.edu.sg>
2022-08-01 12:44:05 +0800

    comments

--- project_group/api.py
+++ project_group/api.py
@@ -1,21 +1,23 @@
 import requests
+
+#calling the program to an API
 api_key = "71RDX3LKNAQLVGYO"
 url = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey=71RDX3LKNAQLVGYO"
-
+#sends requests to a specific url
 response = requests.get(url)
-# print(response)
-# print(response.headers)
 
-# print(response)
+#returns a JSON object of the result
 import json 
 data = response.json()
 # print(float(data["Time Series FX (Weekly)"]["2022-07-29"]["4. close"]))
 
+#data structure to store collections of data
 list = []
 for week, forex in data["Time Series FX (Weekly)"].items():
     closing_price=(float(forex["4. close"]))
     list.append(closing_price)
-    
+
+ #calculating the mean of the weekly closing forex price   
 average_forex_price = sum(list)/len(list)
 print(f"The mean of the weekly closing forex price is ${round(average_forex_price,2)}")
 
--- project_group/profit_loss.py
+++ project_group/profit_loss.py
@@ -8,6 +8,8 @@ with open (r"\Boss_baby_PFB\Boss-Baby-1\project_group\csv_reports\Profit and Los
     for line in reader:
         # print(line[0], line[1], line[2], line[3], line[4])
         Profit_and_lose.append(line)
+
+    
 print(Profit_and_lose)
 print()
 
