33e1e99e7f7b54815645d3c89c7ad85976c1ae4d
kahfon <S10220869@connect.np.edu.sg>
2022-08-01 13:27:21 +0800

    cash on hand

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -9,13 +9,13 @@ with open (r"\Boss_baby_PFB\Boss-Baby-1\project_group\csv_reports\Cash on Hand.c
         # print(line[0],line[1])
         Cash_on_hand.append(line)
     with open("Cash on Hand.csv",'r') as file:
-  reader = csv.reader(file)
-  header = next(reader)
-  for line in reader:
-    Cash_on_hand.append(line)
-  i=0
-  for a in Cash_on_hand:
-    i += 1
-    print(a)
+    #reader = csv.reader(file)
+    #header = next(reader)
+    #for line in reader:
+        #Cash_on_hand.append(line)
+        i=0
+    for a in Cash_on_hand:
+        i += 1
+        print(a)
 print(Cash_on_hand)
 print()
