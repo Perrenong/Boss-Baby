2009ae492322d5bcfdcc20a22fa04c14beff1d82
darylyip <s10247793@connect.np.edu.sg>
2022-08-01 15:13:34 +0800

    flaglist

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -8,14 +8,11 @@ with open (r"\Boss_baby_PFB\Boss-Baby-1\project_group\csv_reports\Cash on Hand.c
     for line in reader:
         # print(line[0],line[1])
         Cash_on_hand.append(line)
-    with open("Cash on Hand.csv",'r') as file:
-  reader = csv.reader(file)
-  header = next(reader)
-  for line in reader:
-    Cash_on_hand.append(line)
-  i=0
-  for a in Cash_on_hand:
-    i += 1
-    print(a)
-print(Cash_on_hand)
-print()
+
+flag_list = []
+if day
+num = Cash_on_hand.append(line)
+prev_figure = flag_list
+flag_list.insert(1, prev_figure) 
+print(flag_list)
+
