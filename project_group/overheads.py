60d9b5875ae2b61e84f6cece0bf869bbd81a3694
kahfon <S10220869@connect.np.edu.sg>
2022-08-01 13:22:47 +0800

    comments

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -8,5 +8,8 @@ with open (r"\Boss_baby_PFB\Boss-Baby-1\project_group\csv_reports\overheads.csv"
     for line in reader:
         # print(line[0], line[1])
         Overheads.append(line)
+
+
+        
 print(Overheads)
 print()
