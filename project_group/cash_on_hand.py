09eac3edadfaa5faa0c8341bbaafea0ee72e446b
darylyip <s10247793@connect.np.edu.sg>
2022-08-01 10:56:10 +0800

    daryl

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -1,2 +1,12 @@
 from pathlib import Path
 import csv
+Cash_on_hand = []
+with open (r"\NP_PFB\Boss-Baby\csv_reports\cash-on-hand-usd.csv", "r") as file:
+    
+    reader = csv.reader(file)
+    next(reader)
+    for line in reader:
+        # print(line[0],line[1])
+        Cash_on_hand.append(line)
+print(Cash_on_hand)
+print()
--- project_group/profit_loss.py
+++ project_group/profit_loss.py
@@ -1,3 +1,13 @@
 from pathlib import Path
 import csv
+Profit_and_lose = []
+with open (r"\NP_PFB\Boss-Baby\csv_reports\profit-and-loss-usd.csv", "r") as file:
+    
+    reader = csv.reader(file)
+    next(reader)
+    for line in reader:
+        # print(line[0], line[1], line[2], line[3], line[4])
+        Profit_and_lose.append(line)
+print(Profit_and_lose)
+print()
 
