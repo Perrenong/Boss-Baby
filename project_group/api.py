94efca3e8671e92ed1b1f3657a7b31e3ae090edd
Perrenong <S10240337@connect.np.edu.sg>
2022-08-04 02:23:47 +0800

    New API file

--- project_group/api.py
+++ project_group/api.py
@@ -1,14 +1,18 @@
 import requests
+import json 
 
-api_key = "71RDX3LKNAQLVGYO"
 url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=71RDX3LKNAQLVGYO"
 
 response = requests.get(url)
-print(response)
-print(response.headers)
-
-print(response)
-import json 
 data = response.json()
-datas = json.dumps (data, indent = 4)
-print(datas)
+
+"""
+get the exchange rate of  1 USD to SGD
+"""
+def get_exchange_rate():
+    # data is type of dictionary
+
+    # ['Realtime Currency Exchange Rate'] is the key the of dictionary
+    # ['5. Exchange Rate'] is the key of the value of the dictionary
+    
+    return data['Realtime Currency Exchange Rate']['5. Exchange Rate']
