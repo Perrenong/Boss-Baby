bb06c80228f5741276a5fc2a43c2149253b9bcae
KYLIELXE <S10240068@connect.np.edu.sg>
2022-08-04 16:38:17 +0800

    final overheads

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -1,21 +1,45 @@
-import csv
+import read_file
 
-Overheads = []
-with open (r"\Boss_baby_PFB\Boss-Baby-1\project_group\csv_reports\overheads.csv", "r") as file:
+"""
+Converts the data based on exchange_rate. -> data * exchange_rate
+"""
+def conversion(exchange_rate):
+    data = read_file.overheads()
+    data.pop(0)
+    for i in range(len(data)):
+        data[i][1] = float(data[i][1]) * float(exchange_rate)
+    return data
+
+"""
+Returns the highest overhead in the data.
+"""
+def highest_overhead(data):
+    # Temporary Storage to track the highest value
+    highest_num = 0
+    highest_index = 0
+
+    # Loop through data and get the highest value and it's index
+    for i in range(len(data)):  
+        if(float(data[i][1]) > highest_num):
+            highest_num = float(data[i][1])
+            highest_index = i
     
-    reader = csv.reader(file)
-    next(reader)
-    for line in reader:
-        # print(line[0], line[1])
-        Overheads.append(line)
+    # save the highest value and it's index in highest_data
+    highest_data = data[highest_index]
 
-category = []
+    # remove the highest value from the data
+    data.pop(highest_index)
 
-highest_expense = "salary_expense"
+    # insert it at index 0 the highest value and it's index
+    data.insert(0, highest_data)
+
+    return data
+
+"""
+Entry Function for Overheads
+"""
+def overheads(exchange_rate):
+    data = conversion(exchange_rate)
+    data = highest_overhead(data)
+    return data
     
-print(f"The highest category is salary_expense, 29.43")
-
-
-
-print(Overheads)
-print()
